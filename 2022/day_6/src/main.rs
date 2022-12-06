1720179e3a60c3492dc1b46b39533d7c53ae40b1
Brandon Bemister <brandon@getmysa.com>
2022-12-06 17:36:32 -0330

    Replaced suboptimal vecdeque solution
    Instead of putting the value in a VecDeque like I originally thought, I
    can just borrow an immutable slice of the previous values.

--- 2022/day_6/src/main.rs
+++ 2022/day_6/src/main.rs
@@ -1,9 +1,8 @@
-use std::collections::VecDeque;
 use std::fs;
 
-fn contains_duplicates(header: &VecDeque<char>) -> bool {
-    for (i, c) in header.iter().enumerate() {
-        for h in header.iter().skip(i + 1) {
+fn contains_duplicates(header: &str) -> bool {
+    for (i, c) in header.chars().enumerate() {
+        for h in header.chars().skip(i + 1) {
             if c == h {
                 return true;
             }
@@ -12,37 +11,20 @@ fn contains_duplicates(header: &VecDeque<char>) -> bool {
     false
 }
 
-fn prefill_pattern_buffer(
-    input_message: &str,
-    pattern_buffer: &mut VecDeque<char>,
-    pattern_size: usize,
-) {
-    // Dummy value, gets popped at the beginning of main loop, did this to avoid extra if statement
-    pattern_buffer.push_back('a');
-    for (i, c) in input_message.chars().enumerate() {
-        pattern_buffer.push_back(c);
-        if i == pattern_size - 2 {
-            break;
-        }
-    }
-}
-
 fn detect_non_repeating_pattern(input_message: &str, pattern_size: usize) -> Option<usize> {
-    if pattern_size <= 3 || input_message.len() <= pattern_size {
+    if input_message.len() <= pattern_size {
         return None;
     }
 
-    let mut pattern_buffer: VecDeque<char> = VecDeque::with_capacity(pattern_size);
-    prefill_pattern_buffer(input_message, &mut pattern_buffer, pattern_size);
-
-    for (i, c) in input_message.chars().skip(pattern_size - 1).enumerate() {
-        pattern_buffer.pop_front();
-        pattern_buffer.push_back(c);
-        if !contains_duplicates(&pattern_buffer) {
-            println!("{:?}", pattern_buffer);
+    for (i, c) in input_message.chars().skip(pattern_size).enumerate() {
+        let pos = i+pattern_size;
+        let header = &input_message[i..pos];
+        if !contains_duplicates(&header) {
+            println!("{:?}", header);
             return Some(i + pattern_size);
         }
     }
+    
     None
 }
 
